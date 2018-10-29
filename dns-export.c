e3d1b0d7cd494a0c7934dc3ad88213e11a16d3f0
rvyslouzil <xvyslo05@stud.fit.vutbr.cz>
2018-10-29 05:12:59 +0100

    Added basic funcionality

--- dns-export.c
+++ dns-export.c
@@ -18,6 +18,7 @@ typedef struct args {
 	char * inputFile;
 	char * interfaceType;
 	char * address;
+	char errbuf[PCAP_ERRBUF_SIZE];
 	int countingTime;
 	int operation;
 } args_t;
@@ -115,6 +116,58 @@ args_t CheckArgs(int argc, char * argv[]) {
 	return a;
 }
 
+void parsePcapFile() {
+
+}
+
+int sniffOnInterface() {
+
+	char port[8] = "port 53";
+	struct bpf_program fp;
+	bpf_u_int32 mask;
+	bpf_u_int32 net;
+
+	pcap_t *handle;
+
+	 if (pcap_lookupnet(var.interfaceType, &net, &mask, var.errbuf) == -1) {
+		 fprintf(stderr, "Can't get netmask for device %s\n", var.interfaceType);
+		 net = 0;
+		 mask = 0;
+	 }
+
+	handle = pcap_open_live(var.interfaceType, BUFSIZ, 1, 1000, var.errbuf);
+	if (handle == NULL) {
+		fprintf(stderr, "Couldn't open device %s: %s\n", var.interfaceType, var.errbuf);
+		return(2);
+	}
+
+	if(pcap_compile(handle, &fp, port, 0, net) == -1) {
+		fprintf(stderr, "Couldn't parse filter %s: %s\n", port, pcap_geterr(handle));
+		return(2);
+	}
+
+	if(pcap_setfilter(handle, &fp) ==  -1) {
+		fprintf(stderr, "Couldn't install filter %s: %s\n", port, pcap_geterr(handle));
+		return(2);
+	}
+
+	if (pcap_datalink(handle) != DLT_EN10MB) {
+		fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", var.interfaceType);
+		return(2);
+	}
+
+	pcap_close(handle);
+	return 0;
+}
+
+void sniffOnAddress() {
+
+}
+
+void sendDataToSyslog() {
+
+}
+
 /*
 Main switch
 
@@ -138,12 +191,15 @@ int main(int argc, char * argv[]) {
 			break;
 		case 2:
 			printf("%s\n", var.inputFile);
+			parsePcapFile();
 			break;
 		case 3:
 			printf("%s\n", var.interfaceType);
+			sniffOnInterface();
 			break;
 		case 4:
 			printf("%s\n", var.address);
+			sniffOnAddress();
 			break;
 	}
 
